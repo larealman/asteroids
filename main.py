ce1e06d3c4c117496b9226abd3538c7f071c09f1
Dariusz Wojewódzki <larealman@gmail.com>
2025-12-06 22:13:13 +0100

    Sure! Pl

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from logger import log_state
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        clock.tick(60)
+
+if __name__ == "__main__":
+    main()
